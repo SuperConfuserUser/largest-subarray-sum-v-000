c248ae3cd9d368725d605773c58df6054f24e8b9
Chely Ho <adept_inept@yahoo.com>
2021-02-12 04:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// function largestSubarraySum(array) {
+//   let max = 0;
+//   let currentSum = 0;
+  
+//   array.forEach(n => {
+//     currentSum += n;
+//     if (max < currentSum) max = currentSum;
+//     if (currentSum < 0) currentSum = 0;
+//   })
+  
+//   return max;
+// }
+
 function largestSubarraySum(array) {
-  let max = 0;
-  let currentSum = 0;
   
-  array.forEach(n => {
-    currentSum += n;
-    if (max < currentSum) max = currentSum;
-    if (currentSum < 0) currentSum = 0;
-  })
-  
-  return max;
 }
